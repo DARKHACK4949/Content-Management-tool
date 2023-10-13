f17a23330c7c70c4bc73616fad745b123733d11d
Sanyam_Sharma98 <89446904+DARKHACK4949@users.noreply.github.com>
2023-10-13 14:21:13 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Get references to HTML elements
+
     const addForm = document.querySelector("#addForm form");
     const titleInput = document.getElementById("title");
     const contentInput = document.getElementById("content");
     const articleTable = document.querySelector("#articleTable table");
     const articleContent = document.getElementById("articleContent");
-  
-    // Load existing articles from localStorage
+
+    
     const storedArticles = JSON.parse(localStorage.getItem("articles")) || [];
   
-    // Update the article table in the HTML
+
     function updateArticleTable() {
       articleTable.innerHTML = `
               <tr>
@@ -19,12 +19,11 @@ document.addEventListener("DOMContentLoaded", function () {
           `;
   
       for (const [index, article] of storedArticles.entries()) {
-        // Create new rows for title and actions
+
         const newRow = articleTable.insertRow();
         const titleCell = newRow.insertCell();
         const actionsCell = newRow.insertCell();
-  
-        // Set title and actions content
+
         titleCell.textContent = article.title;
         titleCell.classList.add("article-title");
   
@@ -35,7 +34,7 @@ document.addEventListener("DOMContentLoaded", function () {
                   </div>
               `;
   
-        // Create a row for content
+      
         const contentRow = articleTable.insertRow();
         const contentCell = contentRow.insertCell();
         contentCell.classList.add("article-content");
@@ -45,33 +44,33 @@ document.addEventListener("DOMContentLoaded", function () {
       }
     }
   
-    // Load articles from localStorage when the page loads
+  
     window.addEventListener("load", function () {
       updateArticleTable();
     });
   
-    // Handle form submission for adding new articles
+    
     addForm.addEventListener("submit", function (event) {
       event.preventDefault();
   
-      // Get input values
+    
       const title = titleInput.value;
       const content = contentInput.value;
   
-      // Add new article to storedArticles array and update localStorage
+ 
       storedArticles.push({ title, content });
       localStorage.setItem("articles", JSON.stringify(storedArticles));
   
-      // Update the article table in the HTML and reset input values
+    
       updateArticleTable();
       titleInput.value = "";
       contentInput.value = "";
     });
   
-    // Handle clicks on the article table
+   
     articleTable.addEventListener("click", function (event) {
       if (event.target.classList.contains("article-title")) {
-        // Toggle display of article content
+        
         const contentCell =
           event.target.parentElement.nextElementSibling.querySelector(
             ".article-content"
@@ -79,48 +78,48 @@ document.addEventListener("DOMContentLoaded", function () {
         contentCell.style.display =
           contentCell.style.display === "none" ? "table-cell" : "none";
       } else if (event.target.classList.contains("btn-edit")) {
-        // Handle edit button click
+        
         const index = event.target.getAttribute("data-index");
         const article = storedArticles[index];
   
-        // Populate form fields with article data
+      
         titleInput.value = article.title;
         contentInput.value = article.content;
   
-        // Remove the edited article and update localStorage
+       
         storedArticles.splice(index, 1);
         localStorage.setItem("articles", JSON.stringify(storedArticles));
   
-        // Update the article table in the HTML
+       
         updateArticleTable();
       } else if (event.target.classList.contains("btn-delete")) {
-        // Handle delete button click
+       
         const index = event.target.getAttribute("data-index");
   
-        // Remove the article and update localStorage
+      
         storedArticles.splice(index, 1);
         localStorage.setItem("articles", JSON.stringify(storedArticles));
   
-        // Update the article table in the HTML
+     
         updateArticleTable();
       }
     });
   
-    // Function to add an element to the article's content
+    
     function addToArticle(content) {
       const element = document.createElement("div");
       element.innerHTML = content;
       articleContent.appendChild(element);
     }
   
-    // Handle click events for adding images
+    
     document.getElementById("addImage").addEventListener("click", (event) => {
       event.preventDefault();
       const input = document.createElement("input");
       input.type = "file";
       input.accept = "image/*";
       input.addEventListener("change", (event) => {
-        // Handle image file selection and display
+  
         const file = event.target.files[0];
         if (file) {
           const reader = new FileReader();
@@ -133,14 +132,14 @@ document.addEventListener("DOMContentLoaded", function () {
       input.click();
     });
   
-    // Handle click events for adding videos
+
     document.getElementById("addVideo").addEventListener("click", (event) => {
       event.preventDefault();
       const input = document.createElement("input");
       input.type = "file";
       input.accept = "video/*";
       input.addEventListener("change", (event) => {
-        // Handle video file selection and display
+    
         const file = event.target.files[0];
         if (file) {
           const reader = new FileReader();
@@ -155,7 +154,7 @@ document.addEventListener("DOMContentLoaded", function () {
       input.click();
     });
   
-    // Handle click events for adding images via URL
+  
     document.getElementById("addImageUrl").addEventListener("click", (event) => {
       event.preventDefault();
       const imageUrl = prompt("Enter image URL:");
@@ -163,8 +162,7 @@ document.addEventListener("DOMContentLoaded", function () {
         addToArticle(`<img src="${imageUrl}" alt="Image">`);
       }
     });
-  
-    // Handle click events for adding videos via URL
+
     document.getElementById("addVideoUrl").addEventListener("click", (event) => {
       event.preventDefault();
       const videoUrl = prompt("Enter video URL:");
@@ -177,7 +175,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     
   
-    // Save content to local storage before leaving the page
+   
     window.addEventListener("beforeunload", () => {
       localStorage.setItem("articles", JSON.stringify(storedArticles));
     });
